07323bd0887b0ac6656ec8769cb81fc1a5fc63fc
KIng22567 <terrencemarapira49@gmail.com>
2025-08-21 21:50:21 +0200

    Add configuration files for logging and web server setup
    - Created appsettings.Development.json with logging configuration.
    - Created appsettings.json with logging and allowed hosts configuration.
    - Added web.config for ASP.NET Core hosting configuration.

--- obj/Release/net9.0/ChessGame.Api.AssemblyInfo.cs
+++ obj/Release/net9.0/ChessGame.Api.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessGame.Api")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cac00714391d0b7e1cb94595b7626d73d1586b0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessGame.Api")]
